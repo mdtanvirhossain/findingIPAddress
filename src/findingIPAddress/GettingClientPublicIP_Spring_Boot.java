0f78ceb2b641942bc74483ce9bd85de64f981e0f
Md. Tanvir Hossain <tanvir15-7157@diu.edu.bd>
2019-09-04 15:39:48 +0600

    Create GettingClientPublicIP_Spring_Boot.java

---
+++ src/findingIPAddress/GettingClientPublicIP_Spring_Boot.java
@@ -0,0 +1,90 @@
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/gettingRemoteInfo")
+public class GettingClientPublicIP_Spring_Boot {
+    @Autowired
+    private HttpServletRequest request;
+
+    @RequestMapping(value = "/host")
+    public String getClientHost() {
+        return request.getRemoteHost();
+    }
+
+    @RequestMapping(value = "/address")
+    public String getClientIP() {
+        return request.getRemoteAddr();
+    }
+
+    //for proxy server or Cloudflare
+    @RequestMapping(value = "/proxy")
+    public String getClientIpProxy(HttpServletRequest request) {
+
+        String remoteAddr = "";
+
+        if (request != null) {
+            remoteAddr = request.getHeader("X-FORWARDED-FOR");
+            if (remoteAddr == null || "".equals(remoteAddr)) {
+                remoteAddr = request.getRemoteAddr();
+            }
+        }
+
+        System.out.println(remoteAddr);
+        return remoteAddr;
+    }
+
+    //another way
+    @RequestMapping(value = "/another")
+    private Map<String, String> getRequestHeadersInMap(HttpServletRequest request) {
+
+        Map<String, String> result = new HashMap<>();
+
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            result.put(key, value);
+        }
+
+        return result;
+    }
+
+    //another2
+    private final String[] IP_HEADER_CANDIDATES = {
+            "X-Forwarded-For",
+            "Proxy-Client-IP",
+            "WL-Proxy-Client-IP",
+            "HTTP_X_FORWARDED_FOR",
+            "HTTP_X_FORWARDED",
+            "HTTP_X_CLUSTER_CLIENT_IP",
+            "HTTP_CLIENT_IP",
+            "HTTP_FORWARDED_FOR",
+            "HTTP_FORWARDED",
+            "HTTP_VIA",
+            "REMOTE_ADDR"};
+
+    @RequestMapping(value = "/another2")
+    public String getClientIpAddressIfServletRequestExist() {
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        for (String header : IP_HEADER_CANDIDATES) {
+            String ipList = request.getHeader(header);
+            if (ipList != null && ipList.length() != 0 && !"unknown".equalsIgnoreCase(ipList)) {
+                String ip = ipList.split(",")[0];
+                return ip;
+            }
+        }
+
+        return request.getRemoteAddr();
+    }
+}
